e4996403e118e040fb5c345e6afe24cf0b7aeaff
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-05-11 22:53:49 -0300

    updated

--- game/croupier.py
+++ game/croupier.py
@@ -12,11 +12,15 @@ class Croupier:
         """Creates the instance of a croupier"""
 
         self.value = 0
+        self.points = 0
     
     def give_number(self):
         """draws a card (gets a random number for the card value)"""
 
         #generates a random value to assign to the value attribute
+       def calculate(self):
         self.value = random.randint(1, 13)
+        self.points = 100 if self.value > first_card and user_input == "h" 100 else self.value < first_card and user_input == "l" else -75
+
     
 
